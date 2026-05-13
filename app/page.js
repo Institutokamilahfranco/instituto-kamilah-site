106c592c33dbdefa812d664c62f4ddeb55aaf616
Institutokamilahfranco <kamilahgomes.comunicacao@gmail.com>
2026-05-13 17:47:56 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -62,7 +62,6 @@ export default function Home() {
                     maxWidth: "800px",
                   }}
                 />
-              </div>
 
               <h1
                 style={{
